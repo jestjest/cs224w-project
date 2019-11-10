1352cda216b620ac12c7190b2f513874bcedf061
Jestin Ma <jestinwith.an.e@gmail.com>
2019-11-10 21:03:04 +0000

    Update dataset paths; make scripts python3 executable by default

--- netx_feat_rolX.py
+++ netx_feat_rolX.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov  1 09:48:14 2019
@@ -20,10 +21,11 @@ import warnings
 
 from graphrole import RecursiveFeatureExtractor, RoleExtractor
 
+DATASET_DIR = '/shared/data'
 
 DATASETS = {
     'basic': [
-        'iran_201901_1_tweets_csv_hashed_1.csv',
+        'iran_201906_1_tweets_csv_hashed.csv',
     ],
 }
 INTERACTIONS = {
@@ -43,8 +45,8 @@ def load_datasets(dataset_grouping):
     """
     li = []
     for dataset in DATASETS[dataset_grouping]:
-        path = './Iran/%s' % dataset
-        #cast column type because some id are of str and others 
+        path = '%s/%s' % (DATASET_DIR, dataset)
+        #cast column type because some id are of str and others
         #float
         type_col={"userid":str,'retweet_userid':str,
                   "in_reply_to_userid":str}
@@ -88,7 +90,7 @@ def generate_network(dataset, graph_out):
                 i += 1
             else:
                 retweet_node_id = userid_to_node_map[retweet_id]
-                
+
             interactions_graph.add_edge(user_node_id, retweet_node_id)
 
         if not pd.isna(reply_id):
@@ -132,7 +134,7 @@ def generate_snap_dataset(
     dataset = load_datasets(dataset_grouping)
     graph= generate_network(dataset, graph_out_path)
     nx.write_gpickle(graph, graph_out_path+".gpickle")
-    
+
     return graph
 
 
@@ -143,53 +145,53 @@ def get_RolX_feat(graph):
 
     Create the node/features matrix for role assignements and for our GNN
     """
-    
+
     feature_extractor = RecursiveFeatureExtractor(g)
     features = feature_extractor.extract_features()
     return features
-    
+
 def get_RolX_roles(features):
     """
     @params: [feat (pandas)]
     @returns: roles for each nodes
 
     Compute roles for every nodes
-    """    
+    """
     role_extractor = RoleExtractor(n_roles=None)
     role_extractor.extract_role_factors(features)
     node_roles = role_extractor.roles
-    
-    #################### if you want to have access to what 
-    #################### are the "probabilitis for each class 
+
+    #################### if you want to have access to what
+    #################### are the "probabilitis for each class
     """
-    print('\nNode role membership by percentage:')
+    print('Node role membership by percentage:')
     print(role_extractor.role_percentage.round(2))
     """
-    
+
     return node_roles
 
 
 def draw_Rolx(g,node_roles):
     """
     @params: [role (str), graph (networkx), node_roles (list)]
-    @returns: 
+    @returns:
 
     Draw the whole graph
-    """  
-    
+    """
+
     unique_roles = sorted(set(node_roles.values()))
-    color_map = ["#9d6d00", "#903ee0", "#11dc79", "#f568ff", "#419500", "#013fb0", 
-          "#f2b64c", "#007ae4", "#ff905a", "#33d3e3", "#9e003a", "#019085", 
-          "#950065", "#afc98f", "#ff9bfa", "#83221d", "#01668a", "#ff7c7c", 
+    color_map = ["#9d6d00", "#903ee0", "#11dc79", "#f568ff", "#419500", "#013fb0",
+          "#f2b64c", "#007ae4", "#ff905a", "#33d3e3", "#9e003a", "#019085",
+          "#950065", "#afc98f", "#ff9bfa", "#83221d", "#01668a", "#ff7c7c",
           "#643561", "#75608a"]
-    
+
     # map roles to colors
     role_colors = {role: color_map[i] for i, role in enumerate(unique_roles)}
     # build list of colors for all nodes in G
     node_colors = [role_colors[node_roles[node]] for node in list_non_0]
-    
+
     plt.figure()
-    
+
     with warnings.catch_warnings():
         # catch matplotlib deprecation warning
         warnings.simplefilter('ignore')
@@ -199,45 +201,45 @@ def draw_Rolx(g,node_roles):
             with_labels=True,
             node_color=node_colors,
         )
-    
+
     return node_roles
 
 def draw_Rolx_per_role(role,g,node_roles):
     """
     @params: [role (str), graph (networkx), node_roles (list)]
-    @returns: 
+    @returns:
 
     Draw the egonet for nodes belonging to role
-    """  
-    
+    """
+
     unique_roles = sorted(set(node_roles.values()))
-    color_map = ["#9d6d00", "#903ee0", "#11dc79", "#f568ff", "#419500", "#013fb0", 
-          "#f2b64c", "#007ae4", "#ff905a", "#33d3e3", "#9e003a", "#019085", 
-          "#950065", "#afc98f", "#ff9bfa", "#83221d", "#01668a", "#ff7c7c", 
+    color_map = ["#9d6d00", "#903ee0", "#11dc79", "#f568ff", "#419500", "#013fb0",
+          "#f2b64c", "#007ae4", "#ff905a", "#33d3e3", "#9e003a", "#019085",
+          "#950065", "#afc98f", "#ff9bfa", "#83221d", "#01668a", "#ff7c7c",
           "#643561", "#75608a"]
-    
+
     list_node_role=[]
     for i in node_roles.keys():
         if(node_roles[i]==role):
             list_node_role+=[i]
-    
+
     #create subgraph with only non role 0
     sub_g=nx.DiGraph()
     sub_g.add_nodes_from(list_node_role)
-    
+
     for edge in g.edges():
         if(sub_g.has_node(edge[0]) and sub_g.has_node(edge[1])):
             sub_g.add_edge(edge[0],edge[1])
-    
-    
-    
+
+
+
     # map roles to colors
     role_colors = {role: color_map[i] for i, role in enumerate(unique_roles)}
     # build list of colors for all nodes in G
     node_colors = [role_colors[node_roles[node]] for node in list_node_role]
-    
+
     plt.figure()
-    
+
     with warnings.catch_warnings():
         # catch matplotlib deprecation warning
         warnings.simplefilter('ignore')
@@ -248,7 +250,7 @@ def draw_Rolx_per_role(role,g,node_roles):
             with_labels=True,
             node_color=node_colors,
         )
-    
+
     return node_roles
 
 
