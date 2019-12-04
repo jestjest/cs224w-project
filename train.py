c86d473987dc7460e5cf76c6aa38a8bb62b8e9f5
JasonZheng20 <jzzheng@stanford.edu>
2019-12-03 22:23:36 -0800

    Added Geometric data loading and training loop with GCN/GAT/GraphSage

--- train.py
+++ train.py
@@ -8,7 +8,9 @@ import argparse
 import models
 import networkx as nx
 import numpy as np
+import pandas as pd
 import pickle as pkl
+from sklearn.model_selection import StratifiedShuffleSplit
 import time
 import torch
 import torch_geometric.data as pyg_d
@@ -37,7 +39,7 @@ def local_arg_parse():
 			help='Gradient clipping.')
 	opt_parser.add_argument('--batch_size', type=int, default=32)
 	opt_parser.add_argument('--hidden_dim', type=int, default=32)
-	opt_parser.add_argument('--model_type', type=str, default='GCN')
+	opt_parser.add_argument('--model_type', type=str, default='GraphSage')
 	opt_parser.add_argument('--num_layers', type=int, default=2)
 	opt_parser.add_argument('--dropout', type=float, default=0.5)
 	opt_parser.add_argument('--epochs', type=int, default=500)
@@ -45,8 +47,7 @@ def local_arg_parse():
 	opt_parser.add_argument('--lr', type=float, default=0.01)
 	opt_parser.add_argument('--opt', type=str, default='adam')
 	opt_parser.add_argument('--opt_scheduler', type=str, default='none')
-	# 'model_type': 'RGCN', 'dataset': 'enzymes', 'num_layers': 2, 'batch_size': 32, 'hidden_dim': 32, 'dropout': 0.5,
-	 # 'epochs': 500, 'opt': 'adam', 'opt_scheduler': 'none', 'opt_restart': 0, 'weight_decay': 5e-3, 'lr': 0.01}
+	opt_parser.add_argument('--flag_index', type=str, default='hate')
 	args = opt_parser.parse_args()
 	return args
 
@@ -59,15 +60,15 @@ def load_hate(features='hate/users_hate_all.content', edges='hate/users.edges', 
 			as a dictionary of n1 to n2.
 	"""
 	num_feats = num_features
-	feat_data = np.zeros((NUM_NODES, num_feats))
-	labels = np.empty((NUM_NODES, 1), dtype=np.int64)
+	feat_data = torch.zeros((NUM_NODES, num_feats))
+	labels = torch.empty((NUM_NODES, 1), dtype=torch.long)
 	node_map = {}
 	label_map = {}
 
 	with open(features) as fp:
 		for i, line in enumerate(fp):
 			info = line.strip().split()
-			feat_data[i, :] = list(map(float, info[1:-1]))
+			feat_data[i, :] = torch.tensor(list(map(float, info[1:-1])))
 			node_map[info[0]] = i
 			if not info[-1] in label_map:
 				label_map[info[-1]] = len(label_map)
@@ -84,29 +85,41 @@ def load_hate(features='hate/users_hate_all.content', edges='hate/users.edges', 
 			adj_lists[paper1].add(paper2)
 			adj_lists[paper2].add(paper1)
 
-	edge_tensor = torch.tensor(edge_list, dtype=torch.int32)
+	edge_tensor = torch.tensor(edge_list, dtype=torch.long)
+	labels = labels.squeeze()
 	print('Label meanings: ', label_map)
 	return feat_data, labels, adj_lists, edge_tensor
+
+def get_stratified_batches():
+	if args.flag_index == "hate":
+		df = pd.read_csv("hate/users_anon.csv")
+		df = df[df.hate != "other"]
+		y = torch.tensor([1 if v == "hateful" else 0 for v in df["hate"].values])
+		x = torch.tensor(df["user_id"].values)
+		del df
+
+	else:
+		df = pd.read_csv("suspended/users_anon.csv")
+		np.random.seed(321)
+		df2 = df[df["is_63_2"] == True].sample(668, axis=0)
+		df3 = df[df["is_63_2"] == False].sample(5405, axis=0)
+		df = pd.concat([df2, df3])
+		y = torch.tensor([1 if v else 0 for v in df["is_63_2"].values])
+		x = torch.tensor(df["user_id"].values)
+		del df, df2, df3
+
+	skf = StratifiedShuffleSplit(n_splits=10, test_size=0.2, train_size=0.8, random_state=123)  # Assuming train-test ratio of 0.8
+	return skf, x, y
 
 def get_datasets():
 	"""
 	Gets the dataset for hateful Twitter users
 	"""
+	# mask not multiple datasets
 	feat_data, labels, adj_lists, edges = load_hate()
-	# print(feat_data.shape, labels.shape, len(adj_lists))
 
-	train_data = pyg_d.Data(x=feat_data, edge_index=edges, y=labels, batch=feat_data[:,0])
-	# train_dataset = pyg_d.Dataset('./dataloader/')
-
-
-	train_dataset = train_data
-	# validation_dataset = []
-	# test_dataset = []
-	return {
-		'train': train_dataset,
-		# 'validation': validation_dataset,
-		# 'test': test_dataset
-	}
+	dataset = pyg_d.Data(x=feat_data, edge_index=edges, y=labels, batch=feat_data[:,0])
+	return dataset
 
 def build_optimizer(args, params):
 	weight_decay = args.weight_decay
@@ -131,77 +144,63 @@ def build_optimizer(args, params):
 # ==============================================================================
 # Train
 # ==============================================================================
-def train(datasets, args):
-	train_loader = DataLoader(datasets['train'], batch_size=args.batch_size, shuffle=True)
-	# validation_loader = DataLoader(datasets['validation'], batch_size=args.batch_size, shuffle=True)
-	# test_loader = DataLoader(datasets['test'], batch_size=args.batch_size, shuffle=True)
-
-	# ==========================================================================
-	# TODO: remove this:
-	dataset = datasets['train']
-	# ==========================================================================
+def train(dataset, args):
 	# build model
 	model = models.GNNStack(
-		100386,
-		args.hidden_dim,
-		3,
+		320, # dataset.num_node_features
+		args.hidden_dim,  # args.hidden_dim
+		3,  # dataset.num_classes
 		args
-		# dataset.num_node_features,
-		# args.hidden_dim,
-		# dataset.num_classes,
-		# args,
 	)
 	scheduler, opt = build_optimizer(args, model.parameters())
+	skf, x, y = get_stratified_batches()
 
 	# train
 	validation_accuracy = list()
 	for epoch in range(args.epochs):
 		total_loss = 0
+		avg_test_acc = 0
 		model.train()
-		# ==========================================================================
-		# TODO: remove this:
-		batch = dataset
-		opt.zero_grad()
-		pred = model(batch)
-		label = batch.y
+		# No need to loop over batches since we only have one batch
+		num_splits = 0
+		for train_indices, test_indices in skf.split(x,y):
+			batch = dataset
+			opt.zero_grad()
+			pred = model(batch)
+			label = batch.y
 
-		loss = model.loss(pred, label)
-		loss.backward()
-		opt.step()
-		total_loss += loss.item() * batch.num_graphs
-		# ==========================================================================
-		# for batch in train_loader:
-		# 	opt.zero_grad()
-		# 	pred = model(batch)
-		# 	label = batch.y
-		#
-		# 	loss = model.loss(pred, label)
-		# 	loss.backward()
-		# 	opt.step()
-		# 	total_loss += loss.item() * batch.num_graphs
-		total_loss /= len(loader.dataset)
+			pred = pred[train_indices]
+			label = label[train_indices]
+
+			loss = model.loss(pred, label)
+			loss.backward()
+			opt.step()
+			total_loss += loss.item()
+			num_splits += 1
+
+			if epoch % 2 == 0:
+				test_acc = test(dataset, model, test_indices)
+				avg_test_acc += test_acc
+		total_loss /= num_splits
+		if epoch % 2 == 0:
+			avg_test_acc /= num_splits
+			print(test_acc,   '  test')
 		print(total_loss)
 
-		if epoch % 10 == 0:
-			test_acc = test(test_loader, model)
-			validation_accuracy.append(test_acc)
-			print(test_acc,   '  test')
-	save_to = 'val_%s.pickle' % (args.model_type)
-	with open(save_to, 'wb') as handle:
-		pkl.dump(validation_accuracy, handle)
 
-
-def test(loader, model, is_validation=False):
+def test(dataset, model, test_indices):
 	model.eval()
 
 	correct = 0
-	for data in loader:
-		with torch.no_grad():
-			pred = model(data).max(dim=1)[1]  # max(dim=1) returns values, indices tuple; only need indices
-			label = data.y
-		correct += pred.eq(label).sum().item()
-		total = len(loader.dataset)
-	return correct / total
+	with torch.no_grad():
+		pred = model(dataset).max(dim=1)[1]  # max(dim=1) returns values, indices tuple; only need indices
+		label = dataset.y
+
+	pred = pred[test_indices]
+	label = dataset.y[test_indices]
+
+	correct = pred.eq(label).sum().item()
+	return correct
 
 
 # ==============================================================================
@@ -210,5 +209,5 @@ def test(loader, model, is_validation=False):
 if __name__ == "__main__":
 	args = local_arg_parse()
 
-	datasets = get_datasets()
-	train(datasets, args)
+	dataset = get_datasets()
+	train(dataset, args)
