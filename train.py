591961751529201f98fd8b5a9254262752e0d6f8
JasonZheng20 <jzzheng@stanford.edu>
2019-12-03 19:51:06 -0800

    No batching Data class instead of Dataloader added

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ import numpy as np
 import pickle as pkl
 import time
 import torch
+import torch_geometric.data as pyg_d
 import torch_geometric.nn as pyg_nn
 import torch.optim as optim
 
@@ -26,76 +27,85 @@ NUM_NODES = 100386
 # ==============================================================================
 def local_arg_parse():
 	opt_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	opt_parser.add_argument('--opt', dest='opt', type=str,
-			help='Type of optimizer')
-	opt_parser.add_argument('--opt-scheduler', dest='opt_scheduler', type=str,
-			help='Type of optimizer scheduler. By default none')
-	opt_parser.add_argument('--opt-restart', dest='opt_restart', type=int,
+	opt_parser.add_argument('--opt-restart', dest='opt_restart', type=int, default=0,
 			help='Number of epochs before restart (by default set to 0 which means no restart)')
 	opt_parser.add_argument('--opt-decay-step', dest='opt_decay_step', type=int,
 			help='Number of epochs before decay')
 	opt_parser.add_argument('--opt-decay-rate', dest='opt_decay_rate', type=float,
 			help='Learning rate decay ratio')
-	opt_parser.add_argument('--lr', dest='lr', type=float,
-			help='Learning rate.')
 	opt_parser.add_argument('--clip', dest='clip', type=float,
 			help='Gradient clipping.')
-	opt_parser.add_argument('--weight_decay', type=float,
-			help='Optimizer weight decay.')
 	opt_parser.add_argument('--batch_size', type=int, default=32)
+	opt_parser.add_argument('--hidden_dim', type=int, default=32)
+	opt_parser.add_argument('--model_type', type=str, default='GCN')
+	opt_parser.add_argument('--num_layers', type=int, default=2)
+	opt_parser.add_argument('--dropout', type=float, default=0.5)
+	opt_parser.add_argument('--epochs', type=int, default=500)
+	opt_parser.add_argument('--weight_decay', type=float, default=5e-3)
+	opt_parser.add_argument('--lr', type=float, default=0.01)
+	opt_parser.add_argument('--opt', type=str, default='adam')
+	opt_parser.add_argument('--opt_scheduler', type=str, default='none')
 	# 'model_type': 'RGCN', 'dataset': 'enzymes', 'num_layers': 2, 'batch_size': 32, 'hidden_dim': 32, 'dropout': 0.5,
 	 # 'epochs': 500, 'opt': 'adam', 'opt_scheduler': 'none', 'opt_restart': 0, 'weight_decay': 5e-3, 'lr': 0.01}
 	args = opt_parser.parse_args()
 	return args
 
 def load_hate(features='hate/users_hate_all.content', edges='hate/users.edges', num_features=320):
-    """
-    Returns:
-        NUM_NODES x num_features matrix of features
-        NUM_NODES x 1 matrix of labels (which are printed out)
-        adjacency list of the (directed) edges file (each line being n1 n2 representing n1 -> n2)
-            as a dictionary of n1 to n2.
-    """
-    num_feats = num_features
-    feat_data = np.zeros((NUM_NODES, num_feats))
-    labels = np.empty((NUM_NODES, 1), dtype=np.int64)
-    node_map = {}
-    label_map = {}
+	"""
+	Returns:
+		NUM_NODES x num_features matrix of features
+		NUM_NODES x 1 matrix of labels (which are printed out)
+		adjacency list of the (directed) edges file (each line being n1 n2 representing n1 -> n2)
+			as a dictionary of n1 to n2.
+	"""
+	num_feats = num_features
+	feat_data = np.zeros((NUM_NODES, num_feats))
+	labels = np.empty((NUM_NODES, 1), dtype=np.int64)
+	node_map = {}
+	label_map = {}
 
-    with open(features) as fp:
-        for i, line in enumerate(fp):
-            info = line.strip().split()
-            feat_data[i, :] = list(map(float, info[1:-1]))
-            node_map[info[0]] = i
-            if not info[-1] in label_map:
-                label_map[info[-1]] = len(label_map)
-            labels[i] = label_map[info[-1]]
+	with open(features) as fp:
+		for i, line in enumerate(fp):
+			info = line.strip().split()
+			feat_data[i, :] = list(map(float, info[1:-1]))
+			node_map[info[0]] = i
+			if not info[-1] in label_map:
+				label_map[info[-1]] = len(label_map)
+			labels[i] = label_map[info[-1]]
 
-    adj_lists = defaultdict(set)
-    with open(edges) as fp:
-        for i, line in enumerate(fp):
-            info = line.strip().split()
-            paper1 = node_map[info[0]]
-            paper2 = node_map[info[1]]
-            adj_lists[paper1].add(paper2)
-            adj_lists[paper2].add(paper1)
+	adj_lists = defaultdict(set)
+	edge_list = list()
+	with open(edges) as fp:
+		for i, line in enumerate(fp):
+			info = line.strip().split()
+			paper1 = node_map[info[0]]
+			paper2 = node_map[info[1]]
+			edge_list.append([paper1, paper2])
+			adj_lists[paper1].add(paper2)
+			adj_lists[paper2].add(paper1)
 
-    print('Label meanings: ', label_map)
-    return feat_data, labels, adj_lists
+	edge_tensor = torch.tensor(edge_list, dtype=torch.int32)
+	print('Label meanings: ', label_map)
+	return feat_data, labels, adj_lists, edge_tensor
 
 def get_datasets():
 	"""
 	Gets the dataset for hateful Twitter users
 	"""
-	feat_data, labels, adj_lists = load_hate()
-	print(feat_data.shape, labels.shape, len(adj_lists))
-	train_dataset = []
-	validation_dataset = []
-	test_dataset = []
+	feat_data, labels, adj_lists, edges = load_hate()
+	# print(feat_data.shape, labels.shape, len(adj_lists))
+
+	train_data = pyg_d.Data(x=feat_data, edge_index=edges, y=labels, batch=feat_data[:,0])
+	# train_dataset = pyg_d.Dataset('./dataloader/')
+
+
+	train_dataset = train_data
+	# validation_dataset = []
+	# test_dataset = []
 	return {
 		'train': train_dataset,
-		'validation': validation_dataset,
-		'test': test_dataset
+		# 'validation': validation_dataset,
+		# 'test': test_dataset
 	}
 
 def build_optimizer(args, params):
@@ -123,15 +133,23 @@ def build_optimizer(args, params):
 # ==============================================================================
 def train(datasets, args):
 	train_loader = DataLoader(datasets['train'], batch_size=args.batch_size, shuffle=True)
-	validation_loader = DataLoader(datasets['validation'], batch_size=args.batch_size, shuffle=True)
-	test_loader = DataLoader(datasets['test'], batch_size=args.batch_size, shuffle=True)
+	# validation_loader = DataLoader(datasets['validation'], batch_size=args.batch_size, shuffle=True)
+	# test_loader = DataLoader(datasets['test'], batch_size=args.batch_size, shuffle=True)
 
+	# ==========================================================================
+	# TODO: remove this:
+	dataset = datasets['train']
+	# ==========================================================================
 	# build model
 	model = models.GNNStack(
-		dataset.num_node_features,
+		100386,
 		args.hidden_dim,
-		dataset.num_classes,
-		args,
+		3,
+		args
+		# dataset.num_node_features,
+		# args.hidden_dim,
+		# dataset.num_classes,
+		# args,
 	)
 	scheduler, opt = build_optimizer(args, model.parameters())
 
@@ -140,15 +158,27 @@ def train(datasets, args):
 	for epoch in range(args.epochs):
 		total_loss = 0
 		model.train()
-		for batch in loader:
-			opt.zero_grad()
-			pred = model(batch)
-			label = batch.y
+		# ==========================================================================
+		# TODO: remove this:
+		batch = dataset
+		opt.zero_grad()
+		pred = model(batch)
+		label = batch.y
 
-			loss = model.loss(pred, label)
-			loss.backward()
-			opt.step()
-			total_loss += loss.item() * batch.num_graphs
+		loss = model.loss(pred, label)
+		loss.backward()
+		opt.step()
+		total_loss += loss.item() * batch.num_graphs
+		# ==========================================================================
+		# for batch in train_loader:
+		# 	opt.zero_grad()
+		# 	pred = model(batch)
+		# 	label = batch.y
+		#
+		# 	loss = model.loss(pred, label)
+		# 	loss.backward()
+		# 	opt.step()
+		# 	total_loss += loss.item() * batch.num_graphs
 		total_loss /= len(loader.dataset)
 		print(total_loss)
 
